08fedb2e6eee309fbdb151c16f1390221fc33b48
UdonDa <udoooon0727@gmail.com>
2018-08-11 13:26:31 +0900

    add user registration

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user =  User.new(name: "Examplsasae User", email: "ussasaser@easasaxample.com",
+      password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "email addresses should be saved as lower-case" do
+    @user.save
+    assert @user.valid?
+
+  end
 end
