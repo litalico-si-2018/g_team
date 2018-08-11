c8e1bc6875c4e63ecd050cbda19fd8215fb59f04
UdonDa <udoooon0727@gmail.com>
2018-08-12 00:08:53 +0900

    fix show_by_grade

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
     @article = Article.find_by(id: params[:id])
     @comments = Comment.where(article_id: params[:id])
   end
+
+  def show_by_grade
+  end
 end
