9b6b2442b0a527079f1c2dcd6fee667fd2c0b5eb
Lickey <lickeymut@icloud.com>
2018-08-11 19:40:39 +0900

    [update]コメント表示機能の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,10 @@
 class ArticlesController < ApplicationController
   def show
     @article = Article.find_by(id: params[:id])
-    # DBを作成したらコメントアウトを消す
-    # child_id = @article.child_id
-    # @child = Child.find_by(id: child_id)
-    # user_id = child.user_id
-    # @user = User.find_by(id: user_id)
+    child_id = @article.child_id
+    child = Child.find_by(id: child_id)
+    user_id = child.user_id
+    @user = User.find_by(id: user_id)
 
     @comments = Comment.where(article_id: params[:id])
   end
