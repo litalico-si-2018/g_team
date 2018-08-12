be882ef91880bb61339a2a01da37aff15b7497de
UdonDa <udoooon0727@gmail.com>
2018-08-12 11:07:53 +0900

    add いいね機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_12_004149) do
+ActiveRecord::Schema.define(version: 2018_08_12_015004) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_08_12_004149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "grade"
+    t.integer "likes_count", default: 0, null: false
   end
 
   create_table "children", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
