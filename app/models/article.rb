e9f956f11972235549a671f5ccd9fa5c3131be64
UdonDa <udoooon0727@gmail.com>
2018-08-12 09:45:10 +0900

    add Like relations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :child
   belongs_to :subject
+  has_many :likes, dependent: :destroy
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,6 @@
 class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :article
+  validates :user_id, presence: true
+  validates :article_id, presence: true
 end
